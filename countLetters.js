eebff27741a3e9f1268e5050c748e671f62bb3a4
Adeyemi Atayero <henry.atayero@gmail.com>
2021-11-24 16:20:55 -0600

    updating lotide with countLetters function

---
+++ countLetters.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual}  ===  ${expected}` );
+  }   else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual}  !==  ${expected}` );
+  }
+};
+
+
+const countLetters = function (sntnc) {
+  let char = sntnc.split(" ");
+  let sentence =char.join("");
+
+  let letterCount = {};
+
+  for (const alphabet of sentence) {
+
+    if (sentence[alphabet] !== ' ') {
+
+    } if (letterCount[alphabet]) {
+        letterCount[alphabet] += 1
+    } else {
+        letterCount[alphabet] = 1
+      }
+
+  }
+
+ return letterCount;
+
+};
+
+
+
+
+//TEST
+
+console.log(countLetters('This is the day that the Lord has made'));
+console.log(countLetters('this is a great day to learning coding'));

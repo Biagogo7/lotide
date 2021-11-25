e844f97ebb83a271a8313f9f2fb9333d0b66098a
Adeyemi Atayero <henry.atayero@gmail.com>
2021-11-25 17:44:51 -0600

    updating lotide with map.js

---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = (array1, array2) =>
+array1.length === array2.length && 
+array1.every((v, i) => v === array2[i]);
+
+const assertArraysEqual = function(actual, expected) {
+  let pass = eqArrays(actual, expected)
+  if (pass) {
+    console.log(`✅✅✅Assertion Passed: ${actual}  ===  ${expected}` );
+  }  else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual}  !==  ${expected}` );
+  }
+};
+
+
+
+const map = function(array, callback) {
+
+const results = [];
+
+for (let item of array) {
+  // console.log('item BEFORE: ', item);
+  // console.log('item AFTER: ', callback(item));
+  // console.log('---');
+  results.push(callback(item));
+}
+
+return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const words2 = ["going", "to", "higher", "places", "ade"];
+
+
+const results1 = map(words2, word => word[0]);
+console.log(results1);
+

6d635298cbc2447a5081b9a4a38e4fa033543796
Adeyemi Atayero <henry.atayero@gmail.com>
2021-12-01 07:58:49 -0600

    updating lotide with refactored middleTest.js and tailTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,37 @@
 
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("returns [5] for [4, 5, 6]", () => {
+    assert.deepEqual(middle([4, 5, 6]), [5]);
+  });
+
+  it("returns ['Lighthouse'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(middle(["Hello", "Lighthouse", "Labs"]), ["Lighthouse"]); 
+  });
+
+  it("returns [4] for [1, 2, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 4, 5, 6]), [4]); 
+  });
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
+  });
+
+});
+
+
+
+
+
+
+
+
+/*
+
+
+
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual'); 
 
@@ -7,3 +40,6 @@ console.log('midarray: ', middle([5, 6]))
 console.log('midarray: ', middle([1, 2, 4, 5, 6]))
 console.log('midarray: ', middle([1, 2, 3, 4, 5, 6]))
 //concole.log(assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4])))
+
+
+*/
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,44 @@
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns [5, 6] for [4, 5, 6]", () => {
+    assert.deepEqual(tail([4, 5, 6]), [5, 6]);
+  });
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]); 
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const assertEqual = require('../assertEqual');
 
 const tail = require('../tail');
 
 
-// Test Case: Check the original array
+      // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
-
+*/
 
 
 // const assertEqual = function(actual, expected) {

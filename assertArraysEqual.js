cd1627a031007a82788fa2619c5cc27cccec15ea
Adeyemi Atayero <henry.atayero@gmail.com>
2021-12-01 05:12:18 -0600

    updating lotide with assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,17 @@
-const eqArrays = (array1, array2) =>
-  array1.length === array2.length && 
-  array1.every((v, i) => v === array2[i]);
+const eqArrays = require('./eqArrays');
 
 
+const assertArraysEqual = function(actual, expected) {
+  let pass = eqArrays(actual, expected)
+  if (pass) {
+    console.log(`✅✅✅Assertion Passed: ${actual}  ===  ${expected}` );
+  }   else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual}  !==  ${expected}` );
+  }
+};
 
-  const assertArraysEqual = function(actual, expected) {
-    let pass = eqArrays(actual, expected)
-    if (pass) {
-      console.log(`✅✅✅Assertion Passed: ${actual}  ===  ${expected}` );
-    }   else {
-      console.log(`🛑🛑🛑Assertion Failed: ${actual}  !==  ${expected}` );
-    }
-  };
 
+module.exports = assertArraysEqual;
 
-  //TEST
-  assertArraysEqual(["1", "2", "3"], ["1", "23", "3"]);
+//TEST
+assertArraysEqual(["1", "2", "3"], ["1", "23", "3"]);

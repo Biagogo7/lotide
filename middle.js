fbe507a824bdb0c9113742806599e9fcef8db943
Adeyemi Atayero <henry.atayero@gmail.com>
2021-12-01 05:22:35 -0600

    updating lotide with refactored middle.js

--- middle.js
+++ middle.js
@@ -1,16 +1,7 @@
 
-const eqArrays = (array1, array2) =>
-array1.length === array2.length && 
-array1.every((v, i) => v === array2[i]);
 
-const assertArraysEqual = function(actual, expected) {
-  let pass = eqArrays(actual, expected)
-  if (pass) {
-    console.log(`✅✅✅Assertion Passed: ${actual}  ===  ${expected}` );
-  }  else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual}  !==  ${expected}` );
-  }
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
   
 const middle = function(arrai) {
@@ -30,8 +21,7 @@ const middle = function(arrai) {
   } else if (arrai.length % 2 === 0) {
     mid = Math.floor(arrai.length / 2)
     let mid2 = mid - 1
-    //console.log('mid2: ', mid2)
-    //console.log('mid:', mid)
+   
     midElmnt = x.push(arrai[mid2])
     midElmnt = x.push(arrai[mid])
   }
@@ -40,8 +30,5 @@ const middle = function(arrai) {
 };
 
 
-//TEST
-console.log('midarray: ', middle([5, 6]))
-console.log('midarray: ', middle([1, 2, 4, 5, 6]))
-console.log('midarray: ', middle([1, 2, 3, 4, 5, 6]))
-//concole.log(assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4])))
+module.exports = middle;
+
---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual'); 
+
+//TEST
+console.log('midarray: ', middle([5, 6]))
+console.log('midarray: ', middle([1, 2, 4, 5, 6]))
+console.log('midarray: ', middle([1, 2, 3, 4, 5, 6]))
+//concole.log(assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4])))

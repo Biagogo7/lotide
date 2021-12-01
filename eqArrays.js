277bd490381f03fd6eef24abe284720e48e5fa01
Adeyemi Atayero <henry.atayero@gmail.com>
2021-12-01 05:03:10 -0600

    updating Lotide with eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual}  ===  ${expected}` );
-  }   else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual}  !==  ${expected}` );
-  }
-};
 
 const eqArrays = (array1, array2) => 
   array1.length === array2.length && 
   array1.every((v, i) => v === array2[i]);
 
 
+module.exports = eqArrays;
 
-  //TEST
-  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-  assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+
+
   
